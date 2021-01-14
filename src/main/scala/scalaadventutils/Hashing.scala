6ea5cc75844bc6e1009c02daee7ebe06197d4732
Owen Davies <owen@odavi.es>
2021-01-14 13:10:17 +0000

    Add hashing utilities module

---
+++ src/main/scala/scalaadventutils/Hashing.scala
@@ -0,0 +1,41 @@
+package scalaadventutils
+
+import scala.collection.mutable.ArrayBuffer
+import java.security.MessageDigest
+
+object Hashing {
+
+    val digest = MessageDigest.getInstance("MD5")
+    val lookup = "0123456789abcdef".toCharArray()
+
+    def md5AsString(x: String) =
+        md5(x.getBytes).map("%02x".format(_)).mkString
+
+    def md5AsBytes(x: String) = md5(x.getBytes)
+
+    // A fast, bit-twiddling byte to byte implementation
+    // Inspired by https://stackoverflow.com/a/9855338
+    def md5FromBytes(x: Array[Byte]): Array[Byte] = {
+        val hash: ArrayBuffer[Byte] = ArrayBuffer.fill(x.size * 2)(0)
+
+        val is = (0 until x.size)
+        val js = (0 until x.size * 2 by 2)
+
+        (is zip js).map(n => {
+            val (i, j) = n
+            hash(j)     = lookup((x(i) >> 4) & 0xf).toByte
+            hash(j + 1) = lookup(x(i) & 0xf).toByte
+        })
+
+        digest.digest(hash.toArray)
+    }
+
+    def md5Multi(x: String, times: Int): String = {
+        var hash = md5AsBytes(x)
+
+        (2 to times).foreach(_ => hash = md5FromBytes(hash) )
+        hash.map("%02x".format(_)).mkString
+    }
+
+    private def md5(x: Array[Byte]) = digest.digest(x)
+}
---
+++ src/test/scala/Hashing.scala
@@ -0,0 +1,37 @@
+package scalaadventutils
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class HashingSpec extends AnyFunSuite {
+
+    val string = "abc0"
+
+    test("Hashing: md5") {
+        assertResult("577571be4de9dcce85a041ba0410f29f") {
+            Hashing.md5AsString(string)
+        }
+
+        assertResult("eec80a0c92dc8a0777c619d9bb51e910") {
+            Hashing.md5AsString(Hashing.md5AsString(string))
+        }
+
+        assert(Hashing.md5AsString(string) ==
+               Hashing.md5AsBytes(string).map("%02x".format(_)).mkString)
+    }
+
+    test("Hashing: md5Multi") {
+        assertResult("577571be4de9dcce85a041ba0410f29f") {
+            Hashing.md5Multi(string, 1)
+        }
+
+        assertResult("16062ce768787384c81fe17a7a60c7e3") {
+            Hashing.md5Multi(string, 3)
+        }
+
+        assertResult("a107ff634856bb300138cac6568c0f24") {
+            Hashing.md5Multi(string, 2017)
+        }
+
+        assert(Hashing.md5AsString(string) == Hashing.md5Multi(string, 1))
+    }
+}
