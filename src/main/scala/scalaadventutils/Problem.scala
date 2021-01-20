9a1a68a7525cc5fa43093b819a3ccf6d8e65dd42
Owen Davies <owen@odavi.es>
2021-01-20 11:21:40 +0000

    Add parseInputToListOfLongArray

--- src/main/scala/scalaadventutils/Problem.scala
+++ src/main/scala/scalaadventutils/Problem.scala
@@ -7,12 +7,18 @@ object Problem {
         .next()
         .split(split)
         .toList
-    
+
     def parseInputToListOfIntArray(res: String, split: String) = io.Source
         .fromResource(res)
         .getLines()
         .toList
         .map { _.split(split).toArray.map(_.toInt) }
+
+    def parseInputToListOfLongArray(res: String, split: String) = io.Source
+        .fromResource(res)
+        .getLines()
+        .toList
+        .map { _.split(split).toArray.map(_.toLong) }
 
     def parseInputToList(res: String) = io.Source
         .fromResource(res)
