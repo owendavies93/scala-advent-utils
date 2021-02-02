f0c4c3f746de1caa75106911ebe19008052e09c0
Owen Davies <owen@odavi.es>
2021-02-02 19:29:45 +0000

    Add Profiler

---
+++ src/main/scala/scalaadventutils/Profiler.scala
@@ -0,0 +1,11 @@
+package scalaadventutils
+
+object Profiler {
+    def timeMS[R](block: => R): R = {
+        val t0 = System.nanoTime()
+        val result = block
+        val t1 = System.nanoTime()
+        println("Elapsed time: " + (t1 - t0) / 1000000 + " ms")
+        result
+    }
+}
