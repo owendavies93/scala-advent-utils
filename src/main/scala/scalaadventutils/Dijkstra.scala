345d212db1b2d671a20e7ff503c69d26e906ab39
Owen Davies <owen@odavi.es>
2021-02-14 14:46:27 +0000

    Add stronglyConnectedTree to Dijkstra

--- src/main/scala/scalaadventutils/Dijkstra.scala
+++ src/main/scala/scalaadventutils/Dijkstra.scala
@@ -60,4 +60,21 @@ object Dijkstra {
         ( g: WeightedUndirectedGraph[N]
         , path: List[N]) =
             path.sliding(2).map(edge => g.get(edge(0))(edge(1))).sum
+
+    /*
+        Geneartes a strongly connected tree, basically
+        a pathological case, for benchmarking
+    */
+    def stronglyConnectedTree
+        ( depth: Int)
+        : WeightedUndirectedGraph[Int] = {
+
+        val rnd   = new scala.util.Random
+        val graph = (1 to depth).map(i =>
+            i -> (1 to depth).filterNot(_ == i)
+                             .map(_ -> (1 + rnd.nextInt(11))).toMap
+        ).toMap
+
+        new WeightedUndirectedGraph(graph)
+    }
 }
