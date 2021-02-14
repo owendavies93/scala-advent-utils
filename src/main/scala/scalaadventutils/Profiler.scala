5017e7b6db3ce55ed2140a87f7f33aa4a52a1482
Owen Davies <owen@odavi.es>
2021-02-14 13:15:44 +0000

    Add microsecond timing to profiler

--- src/main/scala/scalaadventutils/Profiler.scala
+++ src/main/scala/scalaadventutils/Profiler.scala
@@ -8,4 +8,12 @@ object Profiler {
         println("Elapsed time: " + (t1 - t0) / 1000000 + " ms")
         result
     }
+
+    def timeMicS[R](block: => R): R = {
+        val t0 = System.nanoTime()
+        val result = block
+        val t1 = System.nanoTime()
+        println("Elapsed time: " + (t1 - t0) / 1000 + " micros")
+        result
+    }
 }
