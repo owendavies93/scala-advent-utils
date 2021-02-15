d7e8bc50a39906084c73ca71cb115ffab19f13eb
Owen Davies <owen@odavi.es>
2021-02-15 08:23:58 +0000

    Revert "Add "fast" Dijkstra implementation using priority maps"
    This reverts commit 234a8a377c8c8586a463026ea750a329b7c53765.

--- src/main/scala/scalaadventutils/Dijkstra.scala
+++ src/main/scala/scalaadventutils/Dijkstra.scala
@@ -1,6 +1,5 @@
 package scalaadventutils
 
-import de.ummels.prioritymap.PriorityMap
 import scala.collection.mutable.Map
 import scala.collection.mutable.Set
 
@@ -35,45 +34,13 @@ object Dijkstra {
         (result, pred)
     }
 
-    def dijkstraFast[N]
-        ( graph: WeightedUndirectedGraph[N]
-        , source: N )
-        : (Map[N, Int], Map[N, N]) = {
-
-        def find
-            ( active: PriorityMap[N, Int]
-            , res: Map[N, Int]
-            , pred: Map[N, N])
-            : (Map[N, Int], Map[N, N]) =
-            if (active.isEmpty) (res, pred)
-            else {
-                val (minNode, minCost) = active.head
-
-                val neighbours = for {
-                    (node, cost) <- graph.get(minNode)
-                        if !res.contains(node) &&
-                           cost + minCost < active.getOrElse(node, Int.MaxValue)
-                } yield node -> (cost + minCost)
-
-                val preds = neighbours.mapValues(_ => minNode)
-
-                find(active.tail ++ neighbours,
-                     res + (minNode -> minCost),
-                     pred ++ preds)
-            }
-
-        find(PriorityMap(source -> 0), Map.empty, Map.empty)
-    }
-
     def shortestPath[N]
         ( graph: WeightedUndirectedGraph[N]
         , source: N
-        , target: N
-        , useFast: Boolean = false)
+        , target: N)
         : List[N] = {
 
-        val pred = if (useFast) dijkstraFast(graph, source)._2
-                   else dijkstra(graph, source)._2
+        val pred = dijkstra(graph, source)._2
 
         def right[N](x: N, pred: Map[N, N]): List[N] = {
             def go(x: N, acc: List[N]): List[N] = {
